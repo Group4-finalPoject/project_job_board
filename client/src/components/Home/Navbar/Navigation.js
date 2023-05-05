a46e0123069357664db896c503fee1ede0eab50e
jshvaron <jshva@icloud.com>
2023-05-04 23:29:11 -0500

    Updates drop down place holders.

--- client/src/components/Home/Navbar/Navigation.js
+++ client/src/components/Home/Navbar/Navigation.js
@@ -14,14 +14,14 @@ function Navigation() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Resume Tool
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Chat Support</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Premium Subscription
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
