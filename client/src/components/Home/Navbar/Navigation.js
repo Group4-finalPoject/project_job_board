dcc81f13b0a771b3aac977cd1f9bd3280ee1457c
jshvaron <jshva@icloud.com>
2023-05-05 00:49:07 -0500

    nav bar edits

--- client/src/components/Home/Navbar/Navigation.js
+++ client/src/components/Home/Navbar/Navigation.js
@@ -7,18 +7,18 @@ function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">GitJobs</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse className='d-flex justify-content-end' id="basic-navbar-nav d-flex justify-content-end">
+          <Nav className="">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+            <Nav.Link href="#link">Profile</Nav.Link>
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">My Applications</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Resume Tool
+                Saved Jobs
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Chat Support</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Resume Tool</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Premium Subscription
