a52f462fb26c201b9e926126fa11472af2f3b71c
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2023-05-10 18:38:00 -0500

    starts front end JWT

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import LogOrSignUp from './components/logOrSignUp';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/HomePage';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 
 function App() {
 
