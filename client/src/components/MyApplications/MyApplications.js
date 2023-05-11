ace7df783f7134c739bb1438c0fe4e5053d780f9
jshvaron <jshva@icloud.com>
2023-05-10 20:49:15 -0500

    testing

--- client/src/components/MyApplications/MyApplications.js
+++ client/src/components/MyApplications/MyApplications.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Navigation from '../Home/Navbar/Navigation';
 
 
 
@@ -9,8 +10,24 @@ function MyApplications(){
 
     return (
         <>
-            <div>
+            <div className='homeBody'>
+                <Navigation/>
                 <h1>My Applications</h1>
+                <Card className='jobPostings'>
+                    <h1>Postings:</h1>
+                <div className='singlePost'>
+                    <Card className='jobPostCard' style={{ width: '18rem', margin: '15px' }}>
+                        <Card.Body>
+                        <Card.Title>JOB NAME</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                     </Card>
+                </div>
+                </Card>
             </div>
         </>
     );
